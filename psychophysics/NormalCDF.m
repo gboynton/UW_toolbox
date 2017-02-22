342dc86889e47c587a59ee0ca95580b8596a7ace
Kelly Chang <kchang4@uw.edu>
2017-02-22 14:06:23 -0800

    editing psychometric functions

--- psychophysics/NormalCDF.m
+++ psychophysics/NormalCDF.m
@@ -1,10 +1,24 @@
-function y = NormalCDF(p,x)
-%y = Normal(p,x)
+function [y] = NormalCDF(params, x)
+% [y] = NormalCDF(params, x)
 %
-%Parameters:  p.s standard deviation
-%             p.u mean
-%             x   intensity values.
+% The Normal Cumulative Function (cdf) based on MATLAB's normcdf.m
+% (see NORMCDF.m)
+%
+% Inputs:
+%   params      A structure contain the parameters of the normal cdf:
+%       u       Mean
+%       s       Standard deviation
+%
+%   x           Intensity values of the stimuli the Normal CDF is a
+%               function of
+%
+% Output:
+%   y           Output of the Normal Cumulative Distribution as a function
+%               of x
 
-%y = NormalCumulative(x,p.u,p.s^2);
-y = normcdf(x,p.u,p.s);
+% Written by G.M. Boynton - 11/13/2007
+% Edited by Kelly Chang - February 13, 2017
 
+%% Evaluate Normal Cumulative Distribution
+
+y = normcdf(x, params.u, params.s);
--- psychophysics/Weibull.m
+++ psychophysics/Weibull.m
@@ -1,14 +1,34 @@
-function y = Weibull(p,x)
-%y = Weibull(p,x)
+function [y] = Weibull(params, x)
+% [y] = Weibull(params, x)
 %
-%Parameters:  p.b slope
-%             p.t threshold yeilding ~80% correct
-%             x   intensity values.
+% The Weibull function based on this equation:
+%
+% k = (-log((1-e)/(1-g)))^(1/b)
+% f(x) = 1 - ((1-g) * exp(-(k*x/t).^b))
+%
+% Where g is performance expected at chance, e is performance level that
+% defines the threshold, b is the slope of the Weibull function, and t is
+% the threshold
+%
+% Inputs:
+%   params      A structure containing the parameters of the Weibull
+%               function:
+%       b       Slope
+%       t       Stimulus intensity threshold as defined by 'params.e'.
+%               When x = 'params.t', then y = 'params.e'
+%       g       Performance expected at chance, proportion
+%       e       Threshold performance, proportion
+%
+%   x           Intensity values of the stimuli
+%
+% Output:
+%   y           Output of the Weibull function as a function of the
+%               intensity values, x
 
-g = 0.5;  %chance performance
-e = (.5)^(1/3);  %threshold performance ( ~80%)
+% Written by G.M. Boynton - 11/13/2007
+% Edited by Kelly Chang - February 13, 2017
 
-%here it is.
-k = (-log( (1-e)/(1-g)))^(1/p.b);
-y = 1- (1-g)*exp(- (k*x/p.t).^p.b);
+%% Evaluate Weibull Function
 
+k = (-log((1-params.e)/(1-params.g)))^(1/params.b);
+y = 1 - ((1-params.g) * exp(-(k*x/params.t).^params.b));
